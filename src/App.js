4d6b5b346b6109109dc337956f0f92753f86c0f8
Suleyman Selcuk <sulomhb@gmail.com>
2022-06-08 23:31:04 +0200

    API-request successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
-
+import Row from "./components/Row/Row";
 function App() {
   return (
     <div className="App">
        <Header/>
+       <Row/>
        <Footer/>
     </div>
   );
--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,12 +1,37 @@
 
 import React from 'react'
+import { weatherAPIConfig } from '../../weatherAPIConfig';
+
+
+async function getWeather(cityName) {
+    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherAPIConfig.key}`
+    try {
+    const fetchWeatherForecast = await fetch(URL)
+    const weatherForecastJSON = await fetchWeatherForecast.json()
+    let result =  `
+        description : ${weatherForecastJSON.weather[0].description},
+        temp : ${weatherForecastJSON.main.temp},
+        wind : ${weatherForecastJSON.wind.speed},
+        humidity : ${weatherForecastJSON.main.humidity},
+    `
+    console.log(result)
+    return result
+    }
+    catch (error) {
+        console.log("Could not find given city");
+        return "Could not find given city"
+    }
+
+    
+}
+
 
 function Row() {
-
-const weatherAPIKey = require("./weatherAPIKey");
-
     return (
-    <div>API-key: {weatherAPIKey.key}</div>
+    <div>
+        <div>API-key: {weatherAPIConfig.key}</div>
+    <div>Result: {getWeather("Skien")}</div>
+    </div>
   )
 }
 
